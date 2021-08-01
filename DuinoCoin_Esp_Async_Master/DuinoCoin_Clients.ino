cbde8045aac52a56db59f741b3b96582ac25a38c
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-08-01 10:04:43 -0300

    Fix ESP32

--- DuinoCoin_Esp_Async_Master/DuinoCoin_Clients.ino
+++ DuinoCoin_Esp_Async_Master/DuinoCoin_Clients.ino
@@ -79,7 +79,7 @@ bool clients_connect(byte i)
 
   ws_sendAll("[" + String(i) + "]" + "Connecting to Duino-Coin server...");
 
-  if (!clients[i].connect(host, port))
+  if (!clients[i].connect(host.c_str(), port))
   {
     Serial.print("[" + String(i) + "]");
     Serial.println("Connection failed.");
