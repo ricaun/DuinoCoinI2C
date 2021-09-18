557a1ffb5a95ea3b56195b774e29fef0f325d25b
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2021-09-18 16:55:07 -0300

    Remove Extra ','

--- DuinoCoin_Esp_Async_Master/DuinoCoin_Esp_Async_Master.ino
+++ DuinoCoin_Esp_Async_Master/DuinoCoin_Esp_Async_Master.ino
@@ -38,7 +38,7 @@ const char* rigIdentifier = "AVR-I2C";  // Change this if you want a custom mine
 #if ESP8266
 #define LED_BUILTIN 2
 #define MINER "AVR I2C v2.7.3"
-#define JOB "AVR,"
+#define JOB "AVR"
 #endif
 
 #if ESP32
